6b7520049b2e109a63b89f749294b584a5d3368b
Vaddepally Ananya <104820851+Ananya1206@users.noreply.github.com>
2022-06-27 17:28:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-/*var video=document.querySelectorAll('video');
 
-video.forEach(play => play.addEventListener('click',()=>{
-  play.classList.toggle('active');
-
-  if(play.paused){
-    play.play();
-  }
-  else{
-    play.pause();
-    play.currentTime=0;
-  }
-}));
-*/
 document.querySelectorAll('.video-container video').forEach(vid=>{
   vid.onclick=()=>{
     document.querySelector('.popup-video').style.display="block";
